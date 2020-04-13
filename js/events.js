dc361f93f0cfec887a8b4c24b43311de5bd6fe5b
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-13 16:45:25 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+    $('.tasty').attr("border-color", "red");
+  });
+}
+
+function pressIt() {
+  $("form").on("keydown", function(key) {
+    if (key.which == 71) {
+      alert("G was pressed");
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
